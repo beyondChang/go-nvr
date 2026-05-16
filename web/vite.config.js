7ef4d8474de1cc56a583a7bc597ade4f3f9acce8
beyondchang <beyondchang@126.com>
2026-05-16 15:44:45 +0800

    优化

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,6 +6,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
+  build: {
+    outDir: '../internal/ui/static',
+    emptyOutDir: true,
+  },
   resolve: {
     alias: {
       $lib: path.resolve('./src/lib'),
